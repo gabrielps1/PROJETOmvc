db813486e4c5fd8627d69ec5815b9aa2c9c0847f
Gabriel <gabrielpereirasilva9@hotmail.com>
2024-03-11 12:00:29 -0300

    Codigo

---
+++ CE/CE/CodeFirst/CodeFirst/Controllers/ConvidadosController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeFirst.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace CodeFirst.Controllers
+{
+    public class ConvidadosController : Controller
+    {
+        private static IList<Convidado> convidados = new List<Convidado>();
+
+
+        public ActionResult Index()
+        {
+            return View(convidados.OrderBy(cat => cat.Id));
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Convidado convidado)
+        {
+            convidados.Add(convidado);
+            convidado.Id = convidados.Select(cat => cat.Id).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(convidados.Where(cat => cat.Id == id).First());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Convidado categoria)
+        {
+            convidados.Remove(convidados.Where(cat => cat.Id == categoria.Id).First());
+            convidados.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int? id)
+        {
+            return View(convidados.Where(cat => cat.Id == id).First());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(convidados.Where(cat => cat.Id == id).First());
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Convidado categoria)
+        {
+            convidados.Remove(convidados.Where(cat => cat.Id == categoria.Id).First());
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult ListarConvidados()
+        {
+            var convidadosConfirmados = convidados.Where(c => c.comparecimento).ToList();
+            return View(convidadosConfirmados);
+        }
+    
+
+    }
+}
---
+++ CE/CE/CodeFirst/CodeFirst/Models/Convidado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace CodeFirst.Models
+
+{
+    public class Convidado
+    {
+        public long Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+        public bool comparecimento { get; set; }
+    }
+}
